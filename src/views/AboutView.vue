30402b412355dab9d57afff7d2a1d32cd3c7db7a
yonsh.xu <yonsh.xu@gmail.com>
2024-05-10 08:54:53 +0800

    iframe postMessage

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>这是主窗口</h1>
   </div>
+
 </template>
 
 <style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- <TheWelcome /> -->
+    555
   </main>
 </template>
